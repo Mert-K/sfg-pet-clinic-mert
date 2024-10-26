b3e4cd4496a1f1ff654373040213a7a5705d9991
Mert Kurt <tyrael91@hotmail.com>
2024-10-26 14:01:57 +0300

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfg_pet_clinic_mert/SfgPetClinicMertApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_pet_clinic_mert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicMertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicMertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfg_pet_clinic_mert/SfgPetClinicMertApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_pet_clinic_mert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicMertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

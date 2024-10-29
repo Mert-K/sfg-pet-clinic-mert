1eb1438a50c078ce71866a89a913645b8df341fc
Mert Kurt <tyrael91@hotmail.com>
2024-10-29 22:25:53 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfg_pet_clinic_mert/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfg_pet_clinic_mert.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/sfg_pet_clinic_mert/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfg_pet_clinic_mert.model;
+
+public class Vet extends Person{
+
+}

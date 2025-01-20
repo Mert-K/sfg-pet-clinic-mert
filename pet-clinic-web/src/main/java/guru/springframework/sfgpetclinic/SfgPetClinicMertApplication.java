7179e17bffd65a9509371cd06ee6c21ab942c672
Mert Kurt <tyrael91@hotmail.com>
2025-01-20 03:22:07 +0300

    Before Maven Release Plugin

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicMertApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicMertApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgPetClinicMertApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicMertApplication.class, args);
 	}
-
 }
